c47b6678f7dba1914b08f031ea7a342c72f41641
Harremking0 <chakibou1@hotmail.com>
2021-05-05 09:25:01 +0100

    worked on the domManipulation modlue to make a styled card with each wheather card , adde some styling through css

--- src/dataLogic.js
+++ src/dataLogic.js
@@ -9,7 +9,7 @@ const getWeatherData = async () => {
 
    const response  = await  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=495c40655917fc17c9f840b125a97e5e`,{mode: 'cors'});
    const data = await response.json();
-
+   console.log(data)
    const newCard = new WeatherCard(data.name,data.main.temp,data.main.feels_like,data.main.humidity);
    cardStorage.push(newCard)
    console.log(cardStorage)  
--- src/domHandler.js
+++ src/domHandler.js
@@ -6,9 +6,54 @@ const container = document.querySelector(".container")
 const createCards = () =>{
     container.innerHTML="";
     cardStorage.forEach(card =>{
-        const cardSkeleton = createHtmlElement("div",card.id,["cardSkeleton"],null);
-        container.appendChild(cardSkeleton);
-        cardSkeleton.textContent=`City : ${card.name}  temparature : ${card.temp}°C feels Like : ${card.feelsLike}°C humidity: ${card.humidity}`
+        const weatherWrapper = createHtmlElement("div",card.id,["weatherWrapper"],null);
+        container.appendChild(weatherWrapper);
+
+        const weatherCard = createHtmlElement("div",card.id,["weatherCard"],null);
+        weatherWrapper.appendChild(weatherCard);
+
+
+
+
+
+        const currentTemp = createHtmlElement("div",card.id,["currentTemp"],null);
+        weatherCard.appendChild(currentTemp);
+
+
+        const temp = createHtmlElement("span",card.id,["temp"],"25");
+        currentTemp.appendChild(temp);
+
+        const location = createHtmlElement("span",card.id,["location"],"brusells");
+        currentTemp.appendChild(location);
+
+
+
+
+        const currentWeather = createHtmlElement("div",card.id,["currentWeather"],null);
+        weatherCard.appendChild(currentWeather);
+
+
+
+        const conditions = createHtmlElement("span",card.id,["conditions"],null);
+        currentWeather.appendChild(conditions);
+
+        const img = createHtmlElement("img",card.id,["logo"],null);
+        conditions.appendChild(img);
+        
+        const info = createHtmlElement("div",card.id,["info"],null);
+        currentWeather.appendChild(info);
+
+        const rain = createHtmlElement("span",card.id,["rain"],"26");
+        info.appendChild(rain);
+
+        const wind = createHtmlElement("span",card.id,["wind"],"12");
+        info.appendChild(wind);
+
+
+
+
+
+
 
 
     })
